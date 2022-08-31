2e097f38895008e1e950aef476b2c254475aa1eb
techang <techang@techangnoMacBook-Air.local>
2022-08-31 20:56:08 +0900

    first

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
-export default function App() {
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function LoadingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgb(53, 121, 116)'  }}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('./assets/logo.png')}
+      />
+    </View>
+  );
+}
+
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ImageBackground
+       source={require('./assets/本棚.png')} resizeMode="cover"style={styles.image}
+      >
+        <Text>Inside</Text>
+      </ImageBackground>
+      </View>
+  );
+}
+
+
+
+export default function App() {
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => setTimeout(() => setLoading(false), 1000), []);
+
+  if(loading)
+    return(<LoadingScreen />)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +65,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: '100%',
+    height: 250
+  },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+
 });
+
